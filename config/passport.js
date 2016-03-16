552d311675142bc038cfc776d20ad444815cddec
Ramesh Kosalairaman <ramesh03k@gmail.com>
2016-03-16 14:52:50 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -54,7 +54,7 @@ module.exports = function(passport) {
             process.nextTick(function() {
 
                 // find the user in the database based on their facebook id
-                User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+                User.findOne(profile.email, function(err, user) {
 
                     // if there is an error, stop everything and return that
                     // ie an error connecting to the database
