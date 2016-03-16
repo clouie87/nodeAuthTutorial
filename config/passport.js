81cbb7ee5c3496db668e6eb545a7e266926bc42c
Ramesh Kosalairaman <ramesh03k@gmail.com>
2016-03-16 13:15:06 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -50,18 +50,26 @@ module.exports = function(passport) {
 
         // facebook will send back the token and profile
         function(token, refreshToken, profile, done) {
-            // find the user in the database based on their facebook id
--                User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
--
--                    // if there is an error, stop everything and return that
--                    // ie an error connecting to the database
--                    if (err)
--                        return done(err);
--
--                    // if the user is found, then log them in
--                    if (user) {
--                        return done(null, user); // user found, return that user
--                    } 
+             // asynchronous
+            process.nextTick(function() {
+
+                // find the user in the database based on their facebook id
+                User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+
+                    // if there is an error, stop everything and return that
+                    // ie an error connecting to the database
+                    if (err)
+                        return done(err);
+
+                    // if the user is found, then log them in
+                    if (user) {
+                        return done(null, user); // user found, return that user
+                    } else {
+                        
+                    }
+
+                });
+            });
         }));
 
     };
