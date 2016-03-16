5f47124aa397c41fb20b5c1a9d4b6641f0e33a00
Ramesh Kosalairaman <ramesh03k@gmail.com>
2016-03-16 18:26:04 +0530

    Update main.js

--- app/public/js/controllers/main.js
+++ app/public/js/controllers/main.js
@@ -1,22 +1,22 @@
-angular.module('todoController', [])
+angular.module('orderController', [])
 
 	// inject the Todo service factory into our controller
-	.controller('mainController', ['$scope','$http','Todos', function($scope, $http, Todos) {
+	.controller('mainController', ['$scope','$http','Order', function($scope, $http, Order) {
 		$scope.formData = {};
 		$scope.loading = true;
 
 		// GET =====================================================================
 		// when landing on the page, get all todos and show them
 		// use the service to get all the todos
-		Todos.get()
+		Order.get()
 			.success(function(data) {
-				$scope.todos = data;
+				$scope.orders = data;
 				$scope.loading = false;
 			});
 
 		// CREATE ==================================================================
 		// when submitting the add form, send the text to the node API
-		$scope.createTodo = function() {
+		$scope.createOrder = function() {
 
 			// validate the formData to make sure that something is there
 			// if form is empty, nothing will happen
@@ -24,27 +24,27 @@ angular.module('todoController', [])
 				$scope.loading = true;
 
 				// call the create function from our service (returns a promise object)
-				Todos.create($scope.formData)
+				Order.create($scope.formData)
 
 					// if successful creation, call our get function to get all the new todos
 					.success(function(data) {
 						$scope.loading = false;
 						$scope.formData = {}; // clear the form so our user is ready to enter another
-						$scope.todos = data; // assign our new list of todos
+						$scope.orders = data; // assign our new list of todos
 					});
 			}
 		};
 
 		// DELETE ==================================================================
 		// delete a todo after checking it
-		$scope.deleteTodo = function(id) {
+		$scope.deleteOrder = function(id) {
 			$scope.loading = true;
 
-			Todos.delete(id)
+			Order.delete(id)
 				// if successful creation, call our get function to get all the new todos
 				.success(function(data) {
 					$scope.loading = false;
-					$scope.todos = data; // assign our new list of todos
+					$scope.orders = data; // assign our new list of todos
 				});
 		};
 	}]);
