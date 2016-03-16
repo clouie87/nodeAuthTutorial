fbb9b38531e896b8e4c491e84fad11b066ebc2cb
Ramesh Kosalairaman <ramesh03k@gmail.com>
2016-03-16 12:58:03 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -50,30 +50,18 @@ module.exports = function(passport) {
 
         // facebook will send back the token and profile
         function(token, refreshToken, profile, done) {
-            console.log(profile);
-            return done(null, profile);
-            /* asynchronous
-            process.nextTick(function() {
-
-                // if there is no user found with that facebook id, create them
-                var newUser            = new User();
-                
-                // set all of the facebook information in our user model
-                newUser.facebook.id    = profile.id; // set the users facebook id
-                newUser.facebook.token = token; // we will save the token that facebook provides to the user
-                newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
-                newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
-                
-                // save our user to the database
-                newUser.save(function(err) {
-                if (err)
-                    throw err;
-                
-                // if successful, return the new user
-                return done(null, newUser);
-                });
-            });
-            */
+            // find the user in the database based on their facebook id
+-                User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+-
+-                    // if there is an error, stop everything and return that
+-                    // ie an error connecting to the database
+-                    if (err)
+-                        return done(err);
+-
+-                    // if the user is found, then log them in
+-                    if (user) {
+-                        return done(null, user); // user found, return that user
+-                    } 
         }));
 
     };
