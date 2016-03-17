d1b59617fb955fb2fbd1a1baaf081d16847de9f8
Ramesh Kosalairaman <ramesh03k@gmail.com>
2016-03-17 16:25:56 +0530

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -61,7 +61,7 @@ module.exports = function(app, passport) {
                 }
         
                 // SQL Query > Select Data
-                var query = client.query("SELECT * FROM salesforce.order WHERE accountid='"+loginUser.accountid+\"' ORDER BY id ASC;");
+                var query = client.query("SELECT * FROM salesforce.order WHERE accountid='"+loginUser.accountid+"' ORDER BY id ASC;");
         
                 // Stream results back one row at a time
                 query.on('row', function(row) {
