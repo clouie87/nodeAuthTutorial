62b24caaeaff2dbb9c5167e1adb890f18983ec5e
Ramesh Kosalairaman <ramesh03k@gmail.com>
2016-03-17 15:32:08 +0530

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,5 @@
+var pg = require('pg');
+var conString = process.env.DATABASE_URL;
 //all the routes for our application
 module.exports = function(app, passport) {
 
@@ -41,6 +43,35 @@ module.exports = function(app, passport) {
     app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
+    });
+    app.get('/api/order', function(req, res) {
+        var results = [];
+    
+        // Get a Postgres client from the connection pool
+        pg.connect(conString, function(err, client, done) {
+            // Handle connection errors
+            if(err) {
+              done();
+              console.log(err);
+              return res.status(500).json({ success: false, data: err});
+            }
+    
+            // SQL Query > Select Data
+            var query = client.query("SELECT * FROM order ORDER BY id ASC;");
+    
+            // Stream results back one row at a time
+            query.on('row', function(row) {
+                results.push(row);
+            });
+    
+            // After all data is returned, close connection and return results
+            query.on('end', function() {
+                done();
+                return res.json(results);
+            });
+    
+        });
+    
     });
 };
 
