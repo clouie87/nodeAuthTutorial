13a3ebb7d6032bbf4c1dbd3d740566a95dad5579
Ramesh Kosalairaman <ramesh03k@gmail.com>
2016-03-18 13:30:51 +0530

    Create neworder.js

---
+++ app/public/js/controllers/neworder.js
@@ -0,0 +1,11 @@
+angular.module('orderController', [])
+
+	// inject the Todo service factory into our controller
+	.controller('neworderController', ['$scope','$http','Pricebook', function($scope, $http, Pricebook) {
+    $scope.pricebooks = [];
+		Pricebook.get()
+			.success(function(data) {
+				$scope.pricebooks = data;
+			
+			});
+	}]);
