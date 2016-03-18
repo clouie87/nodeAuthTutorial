f0343c6f755fd57e514a693504640bcd54c5b837
Ramesh Kosalairaman <ramesh03k@gmail.com>
2016-03-18 13:28:33 +0530

    Update order.js

--- app/public/js/services/order.js
+++ app/public/js/services/order.js
@@ -8,4 +8,11 @@ angular.module('orderService', [])
 				return $http.get('/api/order');
 			}
 		}
+	}])
+	.factory('Pricebook', ['$http',function($http) {
+		return {
+			get : function() {
+				return $http.get('/api/pricebook');
+			}
+		}
 	}]);
