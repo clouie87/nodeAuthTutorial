b5ce40a3e0b1ff7daec04ad856af598cfbc616ef
RamKosal <ramesh03k@gmail.com>
2016-03-20 01:33:01 +0530

    added view files path

--- server.js
+++ server.js
@@ -15,11 +15,12 @@ var session      = require('express-session');
 
 require('./server/config/passport')(passport); // pass passport for configuration
 
+
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
-
+app.set('views', './server/views');
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
