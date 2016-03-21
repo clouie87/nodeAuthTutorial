89f85085e31aba234a6bf0759401d183e4c0111b
Ramesh Kosalairaman <ramesh03k@gmail.com>
2016-03-21 14:52:47 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,22 @@
 //will hold our database connection settings
+var promise = require('bluebird'); // or any other Promise/A+ compatible library;
+var options = {
+    promiseLib: promise // overriding the default (ES6 Promise);
+};
+var pgp = require('pg-promise')(options);
+// See also: https://github.com/vitaly-t/pg-promise#initialization-options
+
+// Database connection details;
+var cn = {
+    host: 'ec2-54-225-151-64.compute-1.amazonaws.com', // 'localhost' is the default;
+    port: 5432, // 5432 is the default;
+    database: 'd24n3l2kj40397',
+    user: 'lphzusjdgyphib',
+    password: 'iIxxi0Ptg3_FWhPeHWkJ09n4tH'
+};
+// You can check for all default values in:
+// https://github.com/brianc/node-postgres/blob/master/lib/defaults.js
+
 module.exports = {
-
-    'pg' : 'postgres://carolinelouie@localhost/auth' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
+    db = pgp(cn); // database instance;
 };
