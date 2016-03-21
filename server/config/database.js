6f7e92c886025a4fee006715dad2880723933fee
Ramesh Kosalairaman <ramesh03k@gmail.com>
2016-03-21 16:47:43 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,9 +1,5 @@
-//will hold our database connection settings
-var promise = require('bluebird'); // or any other Promise/A+ compatible library;
-var options = {
-    promiseLib: promise // overriding the default (ES6 Promise);
-};
-var pgp = require('pg-promise')(options);
+
+var pgp = require('./pgpromise.js');
 // See also: https://github.com/vitaly-t/pg-promise#initialization-options
 
 // Database connection details;
@@ -18,4 +14,3 @@ var cn = {
 // https://github.com/brianc/node-postgres/blob/master/lib/defaults.js
 var db = pgp(cn); // database instance;
 module.exports = db;
-module.exports = pgp;
