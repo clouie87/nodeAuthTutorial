a76808b3101055ad3baba9314bf572a12d7a7f39
Ramesh Kosalairaman <ramesh03k@gmail.com>
2016-03-21 19:31:52 +0530

    Update neworder.js

--- public/js/controllers/neworder.js
+++ public/js/controllers/neworder.js
@@ -8,25 +8,22 @@ angular.module('orderController', [])
 		.success(function(data) {
 			$scope.pricebooks = data;
 		});
-	}]).directive('autocomplete', function($timeout) {
-	        return function(scope, iElement, iAttrs) {
-	            iElement.autocomplete({
-	                source: scope[iAttrs.uiItems],
-	                focus: function(event,ui) {
-	                    iElement.val(ui.item.email);
-	                    return false;
-	                },
-	                select: function(event, ui) {
-	                        iElement.val(ui.item.email);
-	                        return false;
-	                      //  iElement.trigger('input');
-	                       // iElement.trigger('submit');
-	                }
-	            }).data("autocomplete")._renderItem = function(ul, item) {
-	                return $("<li class='slds-lookup__item'>")
-	                    .data( "item.autocomplete", item )
-	                    .append('<a href="#" role="option"><svg aria-hidden="true" class="slds-icon slds-icon-standard-account slds-icon--small"><use xlink:href="/icons/standard-sprite/svg/symbols.svg#account"></use></svg>'+item.name+'</a>')
-	                    .appendTo('#lookup-menu');
-	            };
-	        }
-    });
+	}]).directive('autoComplete', function(Pricebook) {
+		return {
+		    restrict: 'A',
+		    link: function(scope, elem, attr, ctrl) {
+		                // elem is a jquery lite object if jquery is not present,
+		                // but with jquery and jquery ui, it will be a full jquery object.
+		        debugger
+		        elem.autocomplete({
+		            source: Pricebook.get() //from your service
+		            minLength: 2
+		        }).data("autocomplete")._renderItem = function(ul, item) {
+		                return $("<li class='slds-lookup__item'>")
+		                    .data( "item.autocomplete", item )
+		                    .append('<a href="#" role="option"><svg aria-hidden="true" class="slds-icon slds-icon-standard-account slds-icon--small"><use xlink:href="/icons/standard-sprite/svg/symbols.svg#account"></use></svg>'+item.name+'</a>')
+		                    .appendTo('#lookup-menu');
+		            };
+		    }
+		};
+	});
