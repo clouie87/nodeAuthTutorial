d1a9711705d1fb54dbaf6e9a7e82b8066de8af53
Ramesh Kosalairaman <ramesh03k@gmail.com>
2016-03-21 19:13:14 +0530

    Update neworder.js

--- public/js/controllers/neworder.js
+++ public/js/controllers/neworder.js
@@ -1,11 +1,32 @@
-angular.module('orderController', ['ngTagsInput'])
+angular.module('orderController', [])
 
 	// inject the Todo service factory into our controller
 	.controller('neworderController', ['$scope','$http','Pricebook', function($scope, $http, Pricebook) {
-    $scope.pricebooks = [];
+		$scope.pricebook = null;
+		$scope.pricebooks = [];
 		Pricebook.get()
-			.success(function(data) {
-				$scope.pricebooks = data;
-			
-			});
-	}]);
+		.success(function(data) {
+			$scope.pricebooks = data;
+		});
+	}]).directive('autoComplete', function($timeout) {
+	        return function(scope, iElement, iAttrs) {
+	            iElement.autocomplete({
+	                source: scope[iAttrs.uiItems],
+	                focus: function(event,ui) {
+	                    iElement.val(ui.item.email);
+	                    return false;
+	                },
+	                select: function(event, ui) {
+	                        iElement.val(ui.item.email);
+	                        return false;
+	                      //  iElement.trigger('input');
+	                       // iElement.trigger('submit');
+	                }
+	            }).data("autocomplete")._renderItem = function(ul, item) {
+	                return $("<li class='slds-lookup__item'>")
+	                    .data( "item.autocomplete", item )
+	                    .append('<a href="#" role="option"><svg aria-hidden="true" class="slds-icon slds-icon-standard-account slds-icon--small"><use xlink:href="/icons/standard-sprite/svg/symbols.svg#account"></use></svg>'+item.name+'</a>')
+	                    .appendTo('#lookup-menu');
+	            };
+	        }
+    });
